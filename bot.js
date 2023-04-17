7d609fc4f938b88a092ee882629fa1ed42186d31
Raúl Ágreda <100451269@alumnos.uc3m.es>
2023-04-17 23:57:37 +0200

    added audio

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
+// Token is stored in .env file
 import { config } from 'dotenv';
 import { Client, GatewayIntentBits, NewsChannel } from 'discord.js';
 import { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } from "@discordjs/voice";
-
 config();
 
 Array.random = function (arr) {
@@ -9,14 +9,16 @@ Array.random = function (arr) {
 };
 
 const personalized_audios = {
-  "199930872592334848": ["EPIC_CHOIR_SUSPENSE"], // Ruch
-  "227110468102258688": ["CUQUIN_CURUCUANCUANCUAN"], // Iker
-  "460396771709943808": ["pensar_mas_rapido"], // Drew
-  "511564292064280577": ["EVERYBODY_PUT_YOUR_HANDS_IN_THE_AIR", "dry-fart"], //Daniel
-  "465564985091817483": ["HA_GAAAAY"], // Óscar
-  "510933427277791232": ["me_la_cojo"], // Miau
-  "default": "rickroll",
-  "leave": ["y_se_marcho", "league-of-legends-un-invocador-a-dejado-la-partida"]
+  "discord_id": ["audio1.mp3", "audio2.mp3", "..."],
+  "199930872592334848": ["EPIC_CHOIR_SUSPENSE.mp3"], // Ruch
+  "227110468102258688": ["CUQUIN_CURUCUANCUANCUAN.mp3"], // Iker
+  "460396771709943808": ["pensar_mas_rapido.mp3"], // Drew
+  "511564292064280577": ["EVERYBODY_PUT_YOUR_HANDS_IN_THE_AIR.mp3", "dry-fart.mp3"], //Daniel
+  "465564985091817483": ["HA_GAAAAY.mp3"], // Óscar
+  "648252095098519583": ["squirtle.mp3"], // Lana
+  "510933427277791232": ["me_la_cojo.mp3"], // Miau
+  "default": "rickroll.mp3",
+  "leave": ["y_se_marcho.mp3", "league-of-legends-un-invocador-a-dejado-la-partida.mp3"]
 }
 
 const client = new Client({
@@ -56,7 +58,7 @@ function play_sound(sound)
   if (!voice_channel_connection) return;
   const player = createAudioPlayer();
   console.log("./media/" + sound + ".mp3");
-  const resource = createAudioResource("./media/" + sound + ".mp3");
+  const resource = createAudioResource("./media/" + sound);
   player.play(resource);
   voice_channel_connection.subscribe(player);
   player.on(AudioPlayerStatus.Idle, () => {
@@ -68,11 +70,10 @@ function play_sound(sound)
 client.on('voiceStateUpdate', (oldState, newState) => {
   
   if (oldState.member.user.bot) return;
-  if (newState.channelId !== null && oldState.channelId !== newState.channelId) {
+  if (oldState.channelId !== newState.channelId && newState.channelId !== null) {
     playLateMotiv(newState.member);
   }
-  else if (oldState.channelId !== null && newState.channelId === null)
-  {
+  else if (oldState.channelId !== null && newState.channelId === null) {
     leaveSound(oldState.channel);
   }
 });
