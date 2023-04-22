ca956c381f648d787b04dfeb269fea9c6fe3308a
Raúl Ágreda <100451269@alumnos.uc3m.es>
2023-04-22 18:12:29 +0200

    added Diego

--- personalizedAudios.js
+++ personalizedAudios.js
@@ -3,11 +3,12 @@ const personalized_audios = {
     "199930872592334848": ["to-be-continued-sound-effect-perfect-cut_6aKEVm0.mp3"], // Ruch
     "227110468102258688": ["CUQUIN_CURUCUANCUANCUAN.mp3"], // Iker
     "460396771709943808": ["pensar_mas_rapido.mp3"], // Drew
-    "511564292064280577": ["EVERYBODY_PUT_YOUR_HANDS_IN_THE_AIR.mp3", "dry-fart.mp3"], //Daniel
+    "511564292064280577": ["dry-fart.mp3"], //Daniel
     "465564985091817483": ["hay-viene-el-bombero.mp3"], // Óscar
     "648252095098519583": ["squirtle.mp3"], // Lana
     "510933427277791232": ["me_la_cojo.mp3"], // Miau
     "601798569405710337": ["htler-era-buena-gente-illojuan.mp3"], // Willy
+    "352179033649315840": ["franchesco.mp3"], // Diego
     "default": ["rickroll.mp3"],
     "leave": ["y_se_marcho.mp3", "league-of-legends-un-invocador-a-dejado-la-partida.mp3"]
   }
