e71072734e4a70faad4a810d6bfef9ba5859073d
Raúl Ágreda <100451269@alumnos.uc3m.es>
2023-10-19 22:36:34 +0200

    removed unused functions

--- dbFunctions.js
+++ dbFunctions.js
@@ -2,28 +2,6 @@ const fs = require('fs');
 const { model } = require('./audios.model.js');
 const { connectDB } = require("./db.js");
 
-/**
- * Reads the personalized audios json
- * @returns {Object} the personalized audios json
- */
-function readAudiosJson()
-{
-  if (!fs.existsSync("./personalizedAudios.json"))
-  {
-    fs.writeFileSync("./personalizedAudios.json", "{}");
-  }
-  return JSON.parse(fs.readFileSync("./personalizedAudios.json"));
-}
-
-/**
- * Writes the personalized audios json
- * @param {Object} personalized_audios 
- */
-function writeAudiosJson(personalized_audios)
-{
-  fs.writeFileSync("./personalizedAudios.json", JSON.stringify(personalized_audios, null, 2));
-}
-
 async function getAudios(userId)
 { 
   try
